671e251a8c90c74fa8da0a3c521c651c69c57868
David Park <dpark543107@gmail.com>
2016-03-10 13:08:44 -0600

    added drag and drop

--- project1/script/app.js
+++ project1/script/app.js
@@ -3,14 +3,14 @@
 // top left corner = board[0][0]
 // bottom right corner = board[7][7]
 var board = [
-  ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
-  ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
-  [  '',   '',   '',   '',   '',   '',   '',   ''],
-  [  '',   '',   '',   '',   '',   '',   '',   ''],
-  [  '',   '',   '',   '',   '',   '',   '',   ''],
-  [  '',   '',   '',   '',   '',   '',   '',   ''],
+  ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
   ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
-  ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br']
+  [  '',   '',   '',   '',   '',   '',   '',   ''],
+  [  '',   '',   '',   '',   '',   '',   '',   ''],
+  [  '',   '',   '',   '',   '',   '',   '',   ''],
+  [  '',   '',   '',   '',   '',   '',   '',   ''],
+  ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+  ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']
 ];
 
 // html board
@@ -40,40 +40,79 @@ function boards() {
 function img() {
   // add images
   // White Pieces
-  for (var i = 10; i < 18; i++) {
-    $('.' + i).html('<img class="pieces" src="img/White Pawn.svg" />');
-  }
-  $('.00, .07').html('<img class="pieces" draggable="true" src="img/White Rook.svg" />');
-  $('.01, .06').html('<img class="pieces" draggable="true" src="img/White Knight.svg" />');
-  $('.02, .05').html('<img class="pieces" draggable="true" src="img/White Bishop.svg" />');
-  $('.03').html('<img class="pieces" draggable="true" src="img/White Queen.svg" />');
-  $('.04').html('<img class="pieces" draggable="true" src="img/White King.svg" />');
-  // Black Pieces
   for (var i = 60; i < 68; i++) {
-    $('.' + i).html('<img class="pieces" draggable="true" src="img/Black Pawn.svg" />');
+    // gave id = 60 - 67 to target the white pawn directly
+    $('.' + i).html('<img class="pieces"' + ' ' + 'id=' + i + ' ' + 'src="img/White Pawn.svg" />');
   }
-  $('.70, .77').html('<img class="pieces" draggable="true" src="img/Black Rook.svg" />');
-  $('.71, .76').html('<img class="pieces" draggable="true" src="img/Black Knight.svg" />');
-  $('.72, .75').html('<img class="pieces" draggable="true" src="img/Black Bishop.svg" />');
-  $('.73').html('<img class="pieces" draggable="true" src="img/Black Queen.svg" />');
-  $('.74').html('<img class="pieces" draggable="true" src="img/Black King.svg" />');
-}
-
-////////////////////////////
-// drag and Drop
-////////////////////////////
-$(init);
-function init() {
-  $('.pieces').draggable( {
-    // containment: '.square'
-  });
+  //WHITE = #68-left rook, #69-left knight, #70-left bishop, #71-queen, #72-king,
+  //WHITE = #73-right bishop, #74-right knight, #75-right rook
+  $('.70').html('<img class="pieces"' + ' ' + 'id=' + 68 + ' ' + ' src="img/White Rook.svg" />');
+  $('.71').html('<img class="pieces"' + ' ' + 'id=' + 69 + ' ' + ' src="img/White Knight.svg" />');
+  $('.72').html('<img class="pieces"' + ' ' + 'id=' + 70 + ' ' + ' src="img/White Bishop.svg" />');
+  $('.73').html('<img class="pieces"' + ' ' + 'id=' + 71 + ' ' + ' src="img/White Queen.svg" />');
+  $('.74').html('<img class="pieces"' + ' ' + 'id=' + 72 + ' ' + ' src="img/White King.svg" />');
+  $('.75').html('<img class="pieces"' + ' ' + 'id=' + 73 + ' ' + ' src="img/White Bishop.svg" />');
+  $('.76').html('<img class="pieces"' + ' ' + 'id=' + 74 + ' ' + ' src="img/White Knight.svg" />');
+  $('.77').html('<img class="pieces"' + ' ' + 'id=' + 75 + ' ' + ' src="img/White Rook.svg" />');
+  // Black Pieces
+  for (var i = 10; i < 18; i++) {
+    // gave id = 10-17 to target the black pawn directly
+    $('.' + i).html('<img class="pieces"' + ' ' + 'id=' + i + ' ' + 'src="img/Black Pawn.svg" />');
+  }
+  //BLACK = #9-left rook, #8-left knight, #7-left bishop, #6-queen, #5-king,
+  //BLACK = #4-right bishop, #3-right knight, #2-right rook
+  $('.00').html('<img class="pieces"' + ' ' + 'id=' + 9 + ' ' + ' src="img/Black Rook.svg" />');
+  $('.01').html('<img class="pieces"' + ' ' + 'id=' + 8 + ' ' + ' src="img/Black Knight.svg" />');
+  $('.02').html('<img class="pieces"' + ' ' + 'id=' + 7 + ' ' + ' src="img/Black Bishop.svg" />');
+  $('.03').html('<img class="pieces"' + ' ' + 'id=' + 6 + ' ' + ' src="img/Black Queen.svg" />');
+  $('.04').html('<img class="pieces"' + ' ' + 'id=' + 5 + ' ' + ' src="img/Black King.svg" />');
+  $('.05').html('<img class="pieces"' + ' ' + 'id=' + 4 + ' ' + ' src="img/Black Bishop.svg" />');
+  $('.06').html('<img class="pieces"' + ' ' + 'id=' + 3 + ' ' + ' src="img/Black Knight.svg" />');
+  $('.07').html('<img class="pieces"' + ' ' + 'id=' + 2 + ' ' + ' src="img/Black Rook.svg" />');
 }
 
 ///////////////////
+// drag and drop
+//////////////////
+// change this to directly target pieces!!!!!!!!!!!!!!!!!!!!!!!!!
+// Black pieces id=2-17
+// White pieces id=60-75
+var app = app || {};
+var myIdArray = []
 
-function handleDropEvent( event, ui ) {
-  var draggable = ui.draggable;
-  alert( 'The square with ID "' + draggable.attr('id') + '" was dropped onto me!' );
+function dragAndDrop() {
+  app.piece = $('.pieces');
+  app.square = $('.square');
+
+  console.log(app.piece)
+
+  $(app.piece).on('drag', function(evt) {
+    evt.preventDefault()
+    // console.log(app.piece[0]+ " --------------------------------------")
+    // console.log('drag');
+    console.log($(this))
+    app.piece = $(this)
+  })
+
+  $(app.piece).on('dragend', function(evt) {
+    console.log('dragend');
+  })
+
+  $(app.square).on('dragover', function(evt) {
+    evt.preventDefault();
+    console.log('In the square');
+  })
+
+  $(app.square).on('dragleave', function(evt) {
+    console.log("don't leave!!!");
+  })
+
+  $(app.square).on('drop', function(evt) {
+    evt.preventDefault()
+    $(this).append(app.piece);
+    console.log('dropped!');
+    console.log(evt);
+  })
 }
 
 //////////////////////////////////
@@ -85,6 +124,7 @@ var forwardTwoPawn = function(column, color) {
   // starting position for black
   if (color === 'black') {
     board[1][column] = '';
+    $('.' + 1 + column).children().remove();
     board[3][column] = 'bp';
   // starting position for white
   } else if (color === 'white') {
@@ -126,7 +166,8 @@ $(document).ready(function() {
   console.log("All resources are loaded");
   boards();
   img();
-  init();
+  console.log(board);
+  dragAndDrop();
 });
 
 
